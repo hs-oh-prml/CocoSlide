4ca30d6093cb327337eeb501ad7ff905e0a891f1
dhgudtjr0306 <dhgudtjr0306@gmail.com>
2020-03-03 16:48:05 +0900

    20200303

--- Cocoslide/app/src/main/java/com/cocoslide/MainActivity.kt
+++ Cocoslide/app/src/main/java/com/cocoslide/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.cocoslide
 
+import android.appwidget.AppWidgetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun getData(){
+        var url = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun="
+        var queue = Volley.newRequestQueue(this)
+
+        var postRequest = object: StringRequest(
+            Method.GET, url,
+            object: com.android.volley.Response.Listener<String>{
+                override fun onResponse(response: String?) {
+                    var data = parseHtml(response!!)
+                    var manager = AppWidgetManager.getInstance(this@MainActivity)
+
+                }
+            },
+            object : com.android.volley.Response.ErrorListener{
+                override fun onErrorResponse(error: VolleyError?) {
+//                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    Log.v("Error", error.toString())
+                }
+            }
+        ){}
+        queue.add(postRequest)
+    }
+    fun parseHtml(response: String):ArrayList<String>{
+        var data = ArrayList<String>()
+        var doc = Jsoup.parse(response)
+        var current = doc.getElementsByClass("data_table tbl_scrl_mini2 mgt16")[0]
+        var tr = current.getElementsByTag("tr")
+        for(i in tr){
+            var td = i.getElementsByTag("td")[0]
+            var count = td.text()
+            data.add(count)
+            Log.d("Current", count)
+        }
+        return data
+    }
+
+
 }
--- Cocoslide/app/src/main/java/com/cocoslide/SimpleWidget.kt
+++ Cocoslide/app/src/main/java/com/cocoslide/SimpleWidget.kt
@@ -1,14 +1,23 @@
 package com.cocoslide
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
 import android.widget.RemoteViews
+import android.widget.Toast
+import java.time.LocalDateTime
 
 /**
  * Implementation of App Widget functionality.
  */
 class SimpleWidget : AppWidgetProvider() {
+
+
+    val CLICK = "com.cocoslide.REFRESH_CLICK"
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -16,7 +25,17 @@ class SimpleWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            var intent =Intent(context, WidgetService::class.java)
+//            Log.v("UPDATE", appWidgetId.toString())
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            Log.v("UPDATE", "Foreground")
+                context.startForegroundService(intent)
+            } else {
+                Log.v("UPDATE", "Background")
+                context.startService(intent)
+            }
+//            context.startService(intent)
+//            context.startForegroundService(intent)
         }
     }
 
@@ -27,18 +46,25 @@ class SimpleWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.simple_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.v("Recieve", intent?.action)
+        if(intent?.action == "com.cocoslide.REFRESH_CLICK"){
+            var intent =Intent(context, WidgetService::class.java)
+//            context?.startService(intent)
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                Log.v("UPDATE", "Foreground")
+                context?.startForegroundService(intent)
+            } else {
+                Log.v("UPDATE", "Background")
+                context?.startService(intent)
+            }
+//            context?.startForegroundService(intent)
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+            return
+        } else {
+            super.onReceive(context, intent)
+        }
+
+    }
 }
